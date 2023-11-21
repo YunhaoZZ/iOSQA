2dc0c1190be31f10b82f24955e3bd70dbaebe119
Henry Zhang <zhangenochyun@outlook.com>
2023-11-21 15:38:22 -0600

    new

---
+++ iOSQA/Views/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  iOSQA
+//
+//  Created by HenryZ on 11/6/23.
+//
+
+import SwiftUI
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var docText: UITextView!
+    
+    @IBOutlet weak var questionText: UITextView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
